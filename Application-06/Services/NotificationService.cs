91761252eb36f4ccfb56d959724b7859c4bd1557
Hajira848 <247292@students.au.edu.pk>
2026-05-25 12:31:21 +0500

    add

---
+++ Application-06/Services/NotificationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application_05.Services
+{
+    public class NotificationService
+    {
+        private readonly NotificationConfig _config;
+        public NotificationService(NotificationConfig config)
+        {
+            _config = config;
+        }
+
+        public async Task<List<string>> GetNotificationsAsync(int? numberOfNotifications = null)
+        {
+            int count = numberOfNotifications ?? _config.DefaultNumberOfNotifications;
+
+            await Task.Delay(100);
+
+            var alerts = new List<string>();
+            for (int i = 1; i <= count; i++)
+            {
+                alerts.Add($"Notification system log alert sequence #{i}:" +
+                    $" System performance parameters are running optimal.");
+            }
+
+            return alerts;
+        }
+    }
+}
