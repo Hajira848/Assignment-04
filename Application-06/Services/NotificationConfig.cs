9a8251da578f896315ee123c45ade79b02d736c7
Hajira848 <247292@students.au.edu.pk>
2026-05-25 12:29:55 +0500

    Add Application6.sln

---
+++ Application-06/Services/NotificationConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Application_05.Services 
+{
+    public class NotificationConfig
+    {
+        public int DefaultNumberOfNotifications { get; set; } = 3;
+        public string NotificationStyle { get; set; } = "Detailed"; 
+        public event Action? OnConfigChanged;
+
+        public void NotifyChanged() => OnConfigChanged?.Invoke();
+    }
+}
