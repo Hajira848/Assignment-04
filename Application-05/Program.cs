740e71d96b37a4abc6fce3b43524535da34c3a57
Hajira848 <247292@students.au.edu.pk>
2026-05-25 11:59:44 +0500

    Add

--- Application-05/Program.cs
+++ Application-05/Program.cs
@@ -1,10 +1,14 @@
 using Application_05.Components;
+using Application_05.Services; // Ensures your custom authentication service folder is found
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// ?? EXPLICIT INSTRUCTION: Register the state management service as a global Singleton instance
+builder.Services.AddSingleton<AuthenticationStateService>();
 
 var app = builder.Build();
 
@@ -15,12 +19,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
--- Application-05/Services/AuthenticationStateService.cs
+++ Application-05/Services/AuthenticationStateService.cs
@@ -4,13 +4,9 @@ namespace Application_05.Services
 {
     public class AuthenticationStateService
     {
-        // Property tracking whether the user is logged into the session
         public bool IsAuthenticated { get; private set; } = false;
-
-        // Custom event triggered whenever the user changes their login state
         public event Action? OnStateChanged;
 
-        // Simulates a user logging into the application framework
         public void LogIn()
         {
             if (!IsAuthenticated)
@@ -19,8 +15,6 @@ namespace Application_05.Services
                 NotifyStateChanged();
             }
         }
-
-        // Simulates logging the user out and flushing session flags
         public void LogOut()
         {
             if (IsAuthenticated)
@@ -29,8 +23,6 @@ namespace Application_05.Services
                 NotifyStateChanged();
             }
         }
-
-        // Broadcaster method that alerts all listening components to refresh their UI
         private void NotifyStateChanged() => OnStateChanged?.Invoke();
     }
 }
