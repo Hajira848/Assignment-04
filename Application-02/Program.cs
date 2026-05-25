ba0256387b67322d483f5aad3391cd7d50cfc373
Hajira848 <247292@students.au.edu.pk>
2026-05-25 10:05:29 +0500

    Add Program.cs

--- Application-02/Program.cs
+++ Application-02/Program.cs
@@ -1,27 +1,25 @@
-using Application_02.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapStaticAssets();
-app.MapRazorComponents<App>()
+app.MapRazorComponents<Application_02.Components.App>() 
     .AddInteractiveServerRenderMode();
 
 app.Run();
